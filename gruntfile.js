2cd30c07e2320d8170c9cb460b81c2197c46bd58
Chris Jiang <haoyu000@hotmail.com>
2018-06-01 16:51:33 -0400

    Added UI Kit, grunt scripts setup

--- gruntfile.js
+++ gruntfile.js
@@ -15,8 +15,39 @@ module.exports = function(grunt) {
 					'css/styles.css': 'assets/scss/styles.scss',
 				}
 			}
-		}
+		},
+		//Grunt Uglify (https://www.npmjs.com/package/grunt-contrib-uglify)
+		//Concatenates js files
+		uglify: {
+			my_target: {
+				files: {
+					'js/scripts.js' : [
+						'assets/js/scripts.js',
+						'node_modules/jquery/dist/jquery.js',]
+				}
 
+			}
+		},
+
+		//Grunt Watch (https://www.npmjs.com/package/grunt-contrib-watch)
+		watch: {
+			sass: {
+				files: [
+					'assets/scss/*.scss'
+				],
+				tasks: [
+					'sass'
+				]
+			},
+			scripts: {
+				files: [
+					'assets/js/*.js'
+				],
+				tasks: [
+					'uglify'
+				]
+			}
+		},
 	});
 
 	//load-grunt-tasks loads all packages starting with grunt
@@ -25,7 +56,7 @@ module.exports = function(grunt) {
 
 
 	//My Tasks
-	grunt.registerTask('default', []);
+	grunt.registerTask('default', ['watch']);
 
 
 }
