6c224726e167b4fb353e53ad15b19f8f949440fc
Chris Jiang <haoyu000@hotmail.com>
2018-07-10 23:59:48 -0400

    Tag styling added, Off-canvas side bar added (blank), Minor element adjustments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // style assets
 import '../node_modules/normalize-scss/sass/_normalize.scss'
-import '../node_modules/bourbon/core/_bourbon.scss'
 import '../node_modules/fullpage.js/dist/fullpage.css'
 
 //custom stylesheets
@@ -10,9 +9,18 @@ import './scss/_main-style.scss'
 import $ from 'jQuery'
 window.jQuery = $;
 window.$ = $;
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+// loads the Icon plugin
+UIkit.use(Icons);
 
 // custom script
 $(document).ready(function() {
+
+  // Removes the artificial loading-screen after l33t miliseconds
+  setTimeout(function(){
+    $("#loading-screen").fadeOut();
+  }, 1337);
 
   // aligns on load & recalculate on resize
   alignHeaders();
@@ -38,7 +46,7 @@ $(document).ready(function() {
   function alignDescription(){
     $('div[class^="description"]').each(function(){
       var parentHeight = $(this).siblings('div[class^="header-text"]').height();
-      $(this).css('transform', 'translateY(' + (24 + parentHeight) + 'px)');
+      $(this).css('transform', 'translateY(' + (36 + parentHeight) + 'px)');
     });
   }
 });
